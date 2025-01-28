d29e6ee816be79c4ec7882942d52de02b53ce004
Mohammad Taiyab Khan <mohdtaiyabkhan19@gmail.com>
2025-01-28 12:19:44 +0000

    home.html added

--- app.py
+++ app.py
@@ -10,8 +10,15 @@ app = Flask(__name__)
 # Load the dataset
 df = pd.read_csv("cleaned_geocoded_data.csv")
 
-@app.route("/")
-def index():
+# Home Page Route
+@app.route('/')
+def home():
+    """Home Template with a brief description of the project."""
+    return render_template('home.html')
+
+# Map Page Route
+@app.route('/map')
+def map():
     """
     Renders the map with markers from the dataset.
     """
@@ -25,6 +32,7 @@ def index():
         for _, row in df.iterrows()
     ]
     return render_template("map.html", markers=markers)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
